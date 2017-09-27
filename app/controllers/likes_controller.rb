d809e7a4cb9a7c9eb13cf0da9ebc9891aa389a5c
Regent Chien <regent.chien@hotmail.sg>
2017-09-27 17:23:36 +0800

    like - ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,28 +1,30 @@
 class LikesController < ApplicationController
 
   def create
-    tweet = Tweet.find(params[:tweet_id])
-    like = tweet.likes.create(tweet_id: params[:tweet_id])
-    like.user_id = current_user.id
-    like.save
+    @tweet = Tweet.find(params[:tweet_id])
+    @like = @tweet.likes.create(tweet_id: params[:tweet_id])
+    @like.user_id = current_user.id
+    @like.save
 
     user = current_user
     username, *rest = user.email.split(/@/)
 
-    word = tweet.body.split(/\W+/)
+    word = @tweet.body.split(/\W+/)
     words = word[0] 
     message = 'Your tweet, ' + words + '..., is liked by ' + username + '!'
     tweetuser = Tweet.find(params[:tweet_id]).user_id
     notify = user.notifications.create(notify: message, tweet: tweetuser )
 
-    redirect_back(fallback_location: root_path)
+#   redirect_back(fallback_location: root_path)
     
   end
 
   def destroy
-    like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
-    like.destroy
-    redirect_back(fallback_location: root_path)
+    @tweet = Tweet.find(params[:tweet_id])
+    @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
+    @like.destroy
+
+#  redirect_back(fallback_location: root_path)
   end
 	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20170926014745) do
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "replies", force: :cascade do |t|
     t.text "body"
     t.integer "tweet_id"
