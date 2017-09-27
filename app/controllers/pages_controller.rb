707e589b66274ba225128a77d39d805c108d1a5b
Nazmi <nazmifeeroz@hotmail.com>
2017-09-28 04:41:39 +0800

    fixed tweetlist without sign in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,30 +13,33 @@ class PagesController < ApplicationController
     # Check if any user signed in
     if !user_signed_in?
       redirect_to root_path
+
+    else
+      
+      # Profile pic initialise
+      username = current_user.email
+      @username, *rest = username.split(/@/)
+
+      # Tweet initialise
+      @tweet = Tweet.new
+
+      # Tweets initialise
+      @user = current_user.id
+      @usertweets = Tweet.where(user_id:@user).order(:created_at).reverse
+      @userstweets = Tweet.all.order(:created_at).reverse
+
+      # user tweets count
+      user = current_user.id
+      @tweets = Tweet.where(user_id:user)
+
+      @userfollow = current_user
+
+      @alluser = User.all
+
+      @noti = Notification.where(tweet: @user)
+      
     end
 
-    # Profile pic initialise
-    username = current_user.email
-    @username, *rest = username.split(/@/)
-
-    # Tweet initialise
-    @tweet = Tweet.new
-
-    # Tweets initialise
-    @user = current_user.id
-    @usertweets = Tweet.where(user_id:@user).order(:created_at).reverse
-    @userstweets = Tweet.all.order(:created_at).reverse
-
-    # user tweets count
-    user = current_user.id
-    @tweets = Tweet.where(user_id:user)
-
-    @userfollow = current_user
-
-    @alluser = User.all
-
-    @noti = Notification.where(tweet: @user)
-    
   end
 
 
